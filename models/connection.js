9a51c5d2d6f025c9ad73c42723252cafcd90c9f6
Jessica Maria <jmd.sants@gmail.com>
2018-12-02 04:43:25 -0200

    adicionado arquivo connection.js

---
+++ models/connection.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+//configurando mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/db_internetBanking,",{
+    useMongoClient: true
+}).then(() => {
+    console.log('Banco conectado');
+}).catch((err) => {
+    console.log("Houve um erro ao se conectar ao banco " +err)
+})
