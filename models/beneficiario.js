3f51b3a7c57929530c5a2dc25519c6dc5a9da734
Jessica Maria <jmd.sants@gmail.com>
2018-12-02 17:58:59 -0200

    Beneficiario model alterado

--- models/beneficiario.js
+++ models/beneficiario.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 
 // Model Benificiario
 // Definindo o Model
-const BenificiarioSchema =  mongoose.Schema({ 
-    nmBenificiario: {type: String, required: true},
-    sobrenomeBenificiario: {type: String, required: true},
+const BeneficiarioSchema =  mongoose.Schema({ 
+    nmBeneficiario: {type: String, required: true},
+    sobrenomeBeneficiario: {type: String, required: true},
     nrBanco: {type: Number, required: true},
     nrAgencia: {type: Number, required: true},
     nrConta: {type: Number, required: true},
@@ -14,17 +14,17 @@ const BenificiarioSchema =  mongoose.Schema({
 });
 
 // Collection
-mongoose.model('benificiarios', BenificiarioSchema);
+mongoose.model('beneficiarios', BeneficiarioSchema);
 
 /* const Juliett = mongoose.model('benificiarios') */
 
 /* new Juliett ({
-    nmBenificiario: "Juliett",
-    sobrenomeBenificiario: "Ramos",
+    nmBeneficiario: "Juliett",
+    sobrenomeBeneficiario: "Ramos",
     nrBanco: 0113,
     nrAgencia: 24213,
     nrConta: 7342112,
     nrCPF: 82231321231,
     dsEmail: 'ju.lietem@gmail.com'
-}).save().then(() => {console.log("Benificiário criado com sucesso!")})
-  .catch((err) => {console.log("Houve um erro ao registrar o benificiário: " +err)}) */
+}).save().then(() => {console.log("Beneficiário criado com sucesso!")})
+  .catch((err) => {console.log("Houve um erro ao registrar o beneficiário: " +err)}) */
