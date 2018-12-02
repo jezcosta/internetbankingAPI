96ecd09b5b49cc23f7122581527f69f49a48f9ab
Jessica Maria <jmd.sants@gmail.com>
2018-12-02 10:52:37 -0200

    alterado arquivo usuario.js de models

--- models/usuario.js
+++ models/usuario.js
@@ -1,12 +1,15 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const conn = require('../models/connection.js');
 
+// Model Usuarios
+// Definindo o Model
 const UsuarioSchema =  mongoose.Schema({ 
     nmUsuario: {type: String, required: true},
     sobrenomeUsuario: {type: String, required: true},
     nrBanco: {type: Number, required: true},
     nrAgencia: {type: Number, required: true},
     nrConta: {type: Number, required: true},
-    vlSaldo: {type: Double, required: true},
+    vlSaldo: {type: Number, required: true},
     dsSenha: {type: String, required: true},
     nrCPF: {type: Number, required: true},
     dsEmail: {type: String, required: true}                            
@@ -14,4 +17,18 @@ const UsuarioSchema =  mongoose.Schema({
 
 // Collection
 mongoose.model('usuarios', UsuarioSchema);
-module.exports = usuarios;
+
+// const Osmar = mongoose.model('usuarios')
+
+// new Osmar ({
+//     nmUsuario: "Osmar",
+//     sobrenomeUsuario: "Lúcio",
+//     nrBanco: 011,
+//     nrAgencia: 213,
+//     nrConta: 334112,
+//     vlSaldo: 1500.00,
+//     dsSenha: 1234,
+//     nrCPF: 1223131231,
+//     dsEmail: 'osmar.lucio@gmail.com'
+// }).save().then(() => {console.log("Usuário criado com sucesso!")})
+//   .catch((err) => {console.log("Houve um erro ao registrar o usuário: " +err)})
