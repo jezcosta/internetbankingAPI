78ef6b118b136d278f0e4d30ada0fc4faff0a9be
Jessica Maria <jmd.sants@gmail.com>
2018-12-02 11:23:08 -0200

    beneficiario model alterado.

--- models/beneficiario.js
+++ models/beneficiario.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const conn = require('../models/connection.js');
 
+// Model Benificiario
+// Definindo o Model
 const BenificiarioSchema =  mongoose.Schema({ 
     nmBenificiario: {type: String, required: true},
     sobrenomeBenificiario: {type: String, required: true},
@@ -12,4 +15,16 @@ const BenificiarioSchema =  mongoose.Schema({
 
 // Collection
 mongoose.model('benificiarios', BenificiarioSchema);
-module.exports = benificiarios;
+
+const Denis = mongoose.model('benificiarios')
+
+new Denis ({
+    nmBenificiario: "Denis",
+    sobrenomeBenificiario: "Monte",
+    nrBanco: 013,
+    nrAgencia: 4213,
+    nrConta: 734112,
+    nrCPF: 8223131231,
+    dsEmail: 'deniss.m@gmail.com'
+}).save().then(() => {console.log("Benificiário criado com sucesso!")})
+  .catch((err) => {console.log("Houve um erro ao registrar o benificiário: " +err)})
