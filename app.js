7e038f2a40eef026495a53ae7924e7300d0c6e5b
Jéssica Costa <jessicacosta.dev@hotmail.com>
2018-12-05 12:01:03 -0200

    Alterando opções de CORS para teste

--- app.js
+++ app.js
@@ -10,9 +10,14 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+var corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(bodyparser());
 app.use(express.static(path.join(__dirname, 'public')));
