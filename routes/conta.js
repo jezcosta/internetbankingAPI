1fe5a48fc0faf19eaa56dee381069e4bd3f4aad9
Jessica Maria <jmd.sants@gmail.com>
2018-12-07 10:39:32 -0200

    adicionado rotas extrato, update e delete favorecido

--- routes/conta.js
+++ routes/conta.js
@@ -2,8 +2,13 @@ module.exports = (app) => {
     const { conta } = app.controllers;
     const { autenticacao } = app.middlewares;
 
-    app.post('/api/v1/conta', autenticacao.verificaJWT, conta.getInformacoesConta);
+    app.get('/api/v1/conta', autenticacao.verificaJWT, conta.getInformacoesConta);
+    app.post('/api/v1/conta', autenticacao.verificaJWT, conta.salvaConta);  
     app.post('/api/v1/conta/favorecidos', autenticacao.verificaJWT,conta.getFavorecidos);
     app.post('/api/v1/conta/criar-favorecido', autenticacao.verificaJWT, conta.salvaFavorecido);
-    app.post('/api/v1/extrato', autenticacao.verificaJWT, conta.getExtrato);
+    app.put('/api/v1/conta/atualizar-favorecido/:id', autenticacao.verificaJWT, conta.atualizarFavorecido);
+    app.delete('/api/v1/conta/atualizar-favorecido/:id', autenticacao.verificaJWT, conta.deletarFavorecido);
+    app.get('/api/v1/extrato', autenticacao.verificaJWT, conta.getExtrato);
+    app.post('/api/v1/extrato', autenticacao.verificaJWT, conta.postExtrato);
+
 };
