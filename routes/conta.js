9b3bec08c3c7eb5f37c269d41c3b36fcc0977a76
Jessica Maria <jmd.sants@gmail.com>
2018-12-07 11:03:16 -0200

    adicionada novas rotas

--- routes/conta.js
+++ routes/conta.js
@@ -3,12 +3,13 @@ module.exports = (app) => {
     const { autenticacao } = app.middlewares;
 
     app.get('/api/v1/conta', autenticacao.verificaJWT, conta.getInformacoesConta);
-    app.post('/api/v1/conta', autenticacao.verificaJWT, conta.salvaConta);  
+    app.post('/api/v1/conta', autenticacao.verificaJWT, conta.salvaConta);
+    
     app.post('/api/v1/conta/favorecidos', autenticacao.verificaJWT,conta.getFavorecidos);
     app.post('/api/v1/conta/criar-favorecido', autenticacao.verificaJWT, conta.salvaFavorecido);
-    app.put('/api/v1/conta/atualizar-favorecido/:id', autenticacao.verificaJWT, conta.atualizarFavorecido);
-    app.delete('/api/v1/conta/atualizar-favorecido/:id', autenticacao.verificaJWT, conta.deletarFavorecido);
-    app.get('/api/v1/extrato', autenticacao.verificaJWT, conta.getExtrato);
-    app.post('/api/v1/extrato', autenticacao.verificaJWT, conta.postExtrato);
+    app.put('/api/v1/conta/favorecidos/:id', autenticacao.verificaJWT, conta.atualizarFavorecido);
+    app.delete('/api/v1/conta/favorecidos/:id', autenticacao.verificaJWT, conta.deletarFavorecido);
 
+    app.get('/api/v1/extrato', autenticacao.verificaJWT, conta.getExtrato);
+    app.post('/api/v1/extrato', autenticacao.verificaJWT, conta.salvaExtrato);
 };
