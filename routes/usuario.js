b6449dadaa06a7c5f576d0fbc69f09bb1296d6db
Jéssica Costa <jessicacosta.dev@hotmail.com>
2018-12-08 18:37:30 -0200

    Buscar usuário pelo número da conta

--- routes/usuario.js
+++ routes/usuario.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
     app.post('/api/v1/usuario/logon', usuario.login);
     app.get('/api/v1/usuario/logout', usuario.logout);
     app.post('/api/v1/usuario', autenticacao.verificaJWT, usuario.getInformacoes);
-    app.post('/api/v1/usuario/buscar', usuario.buscarUsuario);
+    app.post('/api/v1/usuario/buscar', autenticacao.verificaJWT, usuario.buscarUsuario);
 
     app.post('/api/v1/usuario/listar', autenticacao.verificaJWT, usuario.listar);
     app.post('/api/v1/usuario/adicionar', autenticacao.verificaJWT, usuario.adicionar);
