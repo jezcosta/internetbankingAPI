69f54cde54bebee0d148236d0c7e1d94873dd51d
Jéssica Costa <jessicacosta.dev@hotmail.com>
2018-12-09 16:50:02 -0200

    Criando arquivo com nome correto

---
+++ models/transacao.js
@@ -0,0 +1,15 @@
+module.exports = () => {
+    const mongoose = require('mongoose');
+    const Schema = mongoose.Schema;
+    
+    const transacao = Schema({
+        tpTransacao: {type: String, enum: ["debito", "credito"]},
+        vlTransacao: {type: Number},
+        dtTransacao: {type: Date, default: Date.now},
+        contaRef: {type: Number},
+        agenciaRef: {type: Number},
+        observacao: {type: String}
+    })
+
+    return mongoose.model('Transacao', transacao);
+}   
