472d5fbaa74cde6588471701e1c003cc7f18347e
Jéssica Costa <jessicacosta.dev@hotmail.com>
2018-12-09 12:21:56 -0200

    Tratando saldo insuficiente na transação

--- controllers/transacao.js
+++ controllers/transacao.js
@@ -10,39 +10,49 @@ module.exports = (app) => {
             const { contaOrigem, agenciaOrigem, contaDestino, agenciaDestino, valor } = req.body;
             const task = Fawn.Task();
             var dadosContaOrigem, dadosContaDestino;
-            
+            var saldoSuficiente = true;
+
             await Conta.findOne({ nrConta: contaOrigem, nrAgencia: agenciaOrigem })
                 .then((dados) => {
-                    dadosContaOrigem = dados;
+                    if(dados) {
+                        dadosContaOrigem = dados;
+                        if((dadosContaOrigem.vlSaldo - valor) < 0) {
+                            saldoSuficiente = false;
+                            retorno.envia(res,400,false,'','Saldo insuficiente',null);
+                        }
+                    }
                 })
                 .catch(function(erro){
                     retorno.envia(res,400,false,erro,'Erro ao procurar conta origem',null);
                 });
             
-            await Conta.findOne({ nrConta: contaDestino, nrAgencia: agenciaDestino })
-                .then((dados) => {
-                    dadosContaDestino = dados;
-                })
-                .catch(function(erro){
-                    retorno.envia(res,400,false,erro,'Erro ao procurar conta destino',null);
-                });
-
-            if(dadosContaOrigem && dadosContaDestino) {
-                task.update("Conta", { nrConta: contaOrigem, nrAgencia: agenciaOrigem }, {$inc: {vlSaldo: -valor}})
-                    .update("Conta", { nrConta: contaDestino, nrAgencia: agenciaDestino }, {$inc: {vlSaldo: valor}})
-                    .run({ useMongoose: true })
-                    .then(function(results){
-                        retorno.envia(res,200,true,null,'Transferência Realizada com Sucesso',null);
+            if(saldoSuficiente) {
+                await Conta.findOne({ nrConta: contaDestino, nrAgencia: agenciaDestino })
+                    .then((dados) => {
+                        dadosContaDestino = dados;
                     })
                     .catch(function(erro){
-                        //rollback
-                        logs.log('error', erro);
-                        retorno.envia(res,400,false,erro,'Erro ao realizar transação',null);
+                        retorno.envia(res,400,false,erro,'Erro ao procurar conta destino',null);
                     });
-            } else {
-                logs.log('error', `"${req.method} ${req.url}" contas: ${contaOrigem}-${agenciaOrigem} / ${contaDestino}-${agenciaDestino} 400 (erro: Conta não encontrada)`);
-                retorno.envia(res,400,false,null,'Conta não encontrada',null);
+
+                if(dadosContaOrigem && dadosContaDestino) {
+                    task.update("Conta", { nrConta: contaOrigem, nrAgencia: agenciaOrigem }, {$inc: {vlSaldo: -valor}})
+                        .update("Conta", { nrConta: contaDestino, nrAgencia: agenciaDestino }, {$inc: {vlSaldo: valor}})
+                        .run({ useMongoose: true })
+                        .then(function(results){
+                            retorno.envia(res,200,true,null,'Transferência Realizada com Sucesso',null);
+                        })
+                        .catch(function(erro){
+                            //rollback
+                            logs.log('error', erro);
+                            retorno.envia(res,400,false,erro,'Erro ao realizar transação',null);
+                        });
+                } else {
+                    logs.log('error', `"${req.method} ${req.url}" contas: ${contaOrigem}-${agenciaOrigem} / ${contaDestino}-${agenciaDestino} 400 (erro: Conta não encontrada)`);
+                    retorno.envia(res,400,false,null,'Conta não encontrada',null);
+                }
             }
+
         }
     };
     return transacaoService;
